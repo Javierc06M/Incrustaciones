d0f5d47c065cda001379bc05f0fc824d34c17de8
Javierc06m <javierc06montoya@gmail.com>
2024-08-23 17:16:30 -0500

    Ejercicio de Incrustacion

---
+++ incrustado-02.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Incrustado-02</title>
+</head>
+<body>
+    <p><?php
+        echo "JAVIER CULQUI MONTOYA";
+    ?></p>
+</body>
+</html>
